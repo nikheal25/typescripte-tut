376981418cbde9465565d21954784f59fe458e57
Nick <nikheal25@gmail.com>
2020-12-14 21:42:42 +1100

    FIRST TS CODE

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-let counter: number = 10;
+// const express = require("express");
+import express from "express";
+const app = express();
+const port = 3000;
 
-console.log(`Counter ${counter}`);
+app.get("/", (req) => {
+  console.log("testing");
+  let a = req.body;
+  //   req.name = "dknkds";
+  // This will cause an error
+});
+
+app.get("/flexible", (req: any) => {
+  req.anything = "can do";
+});
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
